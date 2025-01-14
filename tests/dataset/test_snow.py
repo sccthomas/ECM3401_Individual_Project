e973fc0300e9bfde6ba96240dea9781023447954
sccthomas <95318179+sccthomas@users.noreply.github.com>
2025-01-14 22:32:33 +0000

    Modified caching to cache the tensor object before data augmentations.
    Count can now be overridden using a kwarg *For Training Purposes Only*

--- tests/dataset/test_snow.py
+++ tests/dataset/test_snow.py
@@ -8,7 +8,27 @@ class MyTestCase(unittest.TestCase):
         snow_dataset = SnowDataset(
             dataset_dir_path='/Users/samuelthomas/Documents/University/4thYr_Final'
                              '/ECM3401_Individual_Literature_Review_and_Project/SNOW_Semantic_Segmentation'
-                             '/snow_dataset')
+                             '/snow_dataset'
+        )
+
+        image_1, mask_1 = snow_dataset[0]
+        self.assertTrue(type(image_1).__name__ == 'Tensor')
+        self.assertTrue(type(mask_1).__name__ == 'Tensor')
+        self.assertEqual(image_1.size(), (3, 256, 256))
+        self.assertEqual(mask_1.size(), (1, 256, 256))
+
+        image_2, mask_2 = snow_dataset[0]
+        self.assertTrue(type(image_2).__name__ == 'Tensor')
+        self.assertTrue(type(mask_2).__name__ == 'Tensor')
+        self.assertEqual(image_2.size(), (3, 256, 256))
+        self.assertEqual(mask_2.size(), (1, 256, 256))
+
+        snow_dataset = SnowDataset(
+            dataset_dir_path='/Users/samuelthomas/Documents/University/4thYr_Final'
+                             '/ECM3401_Individual_Literature_Review_and_Project/SNOW_Semantic_Segmentation'
+                             '/snow_dataset',
+            len_override=10000
+        )
 
         image_1, mask_1 = snow_dataset[0]
         self.assertTrue(type(image_1).__name__ == 'Tensor')
