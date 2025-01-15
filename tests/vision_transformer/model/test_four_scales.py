6de8bc728ca91a11e8aa620d0049d3abb11c3769
sccthomas <95318179+sccthomas@users.noreply.github.com>
2025-01-15 22:36:59 +0000

    Created new four and five scale models, including tests.

---
+++ tests/vision_transformer/model/test_four_scales.py
@@ -0,0 +1,30 @@
+import unittest
+
+import torch
+from torchvision.transforms.v2 import Normalize
+
+from src.vision_transformer.model.four_scales import SemanticSegmentationVisionTransformer
+
+
+class TestSemanticSegmentationVisionTransformer(unittest.TestCase):
+    def test_forward(self) -> None:
+        model = SemanticSegmentationVisionTransformer(
+            image_dims=(3, 256, 256),
+            patch_embedding_scale_1=(64, 1024),
+            patch_embedding_scale_2=(32, 512),
+            patch_embedding_scale_3=(16, 256),
+            patch_embedding_scale_4=(8, 128),
+        )
+
+        x = torch.randint(0, 255, (16, 3, 256, 256)).float() / 255
+        norm = Normalize(mean=[0.4808, 0.4178, 0.5046], std=[0.2767, 0.2698, 0.2856])
+        x = norm(x)
+
+        y = model(x)
+
+        self.assertEqual(y.shape, (16, 1, 256, 256))
+        self.assertFalse(torch.isnan(y).any())
+
+
+if __name__ == '__main__':
+    unittest.main()
