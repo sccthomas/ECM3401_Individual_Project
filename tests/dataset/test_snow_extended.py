b5dce5972e59be93343b572da27e124e8ee6244a
sccthomas <95318179+sccthomas@users.noreply.github.com>
2025-01-29 15:16:25 +0000

    New snow dataset extended that rotates images and caches the result. Datasets grow by x4

--- tests/dataset/test_snow_extended.py
+++ tests/dataset/test_snow_extended.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.dataset.snow import SnowDataset
+from src.dataset.snow_extended import SnowDataset
 
 
 class TestSnowDataset(unittest.TestCase):
@@ -8,27 +8,8 @@ class TestSnowDataset(unittest.TestCase):
         snow_dataset = SnowDataset(
             dataset_dir_path='/Users/samuelthomas/Documents/University/4thYr_Final'
                              '/ECM3401_Individual_Literature_Review_and_Project/SNOW_Semantic_Segmentation'
-                             '/snow_dataset'
-
-        )
-
-        image_1, mask_1 = snow_dataset[0]
-        self.assertTrue(type(image_1).__name__ == 'Tensor')
-        self.assertTrue(type(mask_1).__name__ == 'Tensor')
-        self.assertEqual(image_1.size(), (3, 512, 512))
-        self.assertEqual(mask_1.size(), (1, 512, 512))
-
-        image_2, mask_2 = snow_dataset[0]
-        self.assertTrue(type(image_2).__name__ == 'Tensor')
-        self.assertTrue(type(mask_2).__name__ == 'Tensor')
-        self.assertEqual(image_2.size(), (3, 512, 512))
-        self.assertEqual(mask_2.size(), (1, 512, 512))
-
-        snow_dataset = SnowDataset(
-            dataset_dir_path='/Users/samuelthomas/Documents/University/4thYr_Final'
-                             '/ECM3401_Individual_Literature_Review_and_Project/SNOW_Semantic_Segmentation'
                              '/snow_dataset',
-            len_override=10000,
+            len_override=5,
             resize=True,
         )
 
@@ -38,7 +19,7 @@ class TestSnowDataset(unittest.TestCase):
         self.assertEqual(image_1.size(), (3, 256, 256))
         self.assertEqual(mask_1.size(), (1, 256, 256))
 
-        image_2, mask_2 = snow_dataset[0]
+        image_2, mask_2 = snow_dataset[6]
         self.assertTrue(type(image_2).__name__ == 'Tensor')
         self.assertTrue(type(mask_2).__name__ == 'Tensor')
         self.assertEqual(image_2.size(), (3, 256, 256))
