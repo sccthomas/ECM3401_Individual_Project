585ff7059b80da574ebe8368717b0ef0911fa3d1
sccthomas <95318179+sccthomas@users.noreply.github.com>
2025-03-02 12:38:18 +0000

    - Version 2 of contrastive_loss.py

--- src/self_supervised_learning/contrastive_loss.py
+++ src/self_supervised_learning/contrastive_loss.py
@@ -63,12 +63,9 @@ class ContrastivePreTraining(_ssl_base.SelfSupervisedLoss):
             (_nn.Identity(), _T.RandomAdjustSharpness(sharpness_factor=2, p=1)),
             (_T.RandomAdjustSharpness(sharpness_factor=0, p=1), _T.RandomAdjustSharpness(sharpness_factor=2, p=1)),
 
-            (_nn.Identity(), _T.RandomRotation(degrees=90)),
-            (_T.RandomRotation(degrees=90), _nn.Identity()),
-            (_nn.Identity(), _T.RandomRotation(degrees=-90)),
-            (_T.RandomRotation(degrees=-90), _nn.Identity()),
-            (_T.RandomRotation(degrees=90), _T.RandomRotation(degrees=-90)),
-            (_T.RandomRotation(degrees=-90), _T.RandomRotation(degrees=90)),
+            (_nn.Identity(), _T.RandomRotation(degrees=360)),
+            (_T.RandomRotation(degrees=360), _nn.Identity()),
+            (_T.RandomRotation(degrees=360), _T.RandomRotation(degrees=360)),
 
             (_T.RandomErasing(p=1, scale=(0.02, 0.1)), _nn.Identity()),
             (_nn.Identity(), _T.RandomErasing(p=1, scale=(0.02, 0.1))),
